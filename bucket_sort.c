3fd01e5db5c2d8680398c1c2fc4a8c5e2de05029
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2017-10-14 21:56:09 +0530

    program for bucket sort

--- bucket_sort.c
+++ bucket_sort.c
@@ -5,7 +5,7 @@ using namespace std;
  
 void bucketSort(float arr[], int n)
 {
-    //  Create n empty buckets
+ // empty buckets
     vector<float> b[n];
     
     //  Put array elements in different buckets
@@ -19,7 +19,6 @@ void bucketSort(float arr[], int n)
     for (int i=0; i<n; i++)
        sort(b[i].begin(), b[i].end());
  
-    //  Concatenate all buckets into arr[]
     int index = 0;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < b[i].size(); j++)
