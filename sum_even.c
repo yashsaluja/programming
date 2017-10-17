fba357ffd7b24540cdad92272ed41cc71226cc20
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2017-10-17 22:10:13 +0530

    print sum of even numbers

---
+++ sum_even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n,sum;
+    sum=0;
+    clrscr();
+    printf("Enter the Number : ");
+    scanf("%d",&n);
+    for(i=1; i<=n; i++)
+    {
+        if(i%2==0)
+            sum=sum+i;
+    }
+    printf("Sum of all Even Integers is %d",sum);
+    getch();
+}
