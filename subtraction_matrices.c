a0915dfb6fc4b6c66d2fca7683fe86413f8f1015
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2017-10-23 23:13:25 +0530

    program to subtract two matrices

--- subtraction_matrices.c
+++ subtraction_matrices.c
@@ -14,14 +14,10 @@ int main() {
    printf("\nEnter the number of Columns of Mat2 : ");
    scanf("%d", &col2);
 
-   /* Before accepting the Elements Check if no of
-    rows and columns of both matrices is equal */
    if (row1 != row2 || col1 != col2) {
       printf("\nOrder of two matrices is not same ");
       exit(0);
    }
-
-   //Accept the Elements in Matrix 1
    for (i = 0; i < row1; i++) {
       for (j = 0; j < col1; j++) {
          printf("Enter the Element a[%d][%d] : ", i, j);
@@ -29,20 +25,16 @@ int main() {
       }
    }
 
-   //Accept the Elements in Matrix 2
    for (i = 0; i < row2; i++)
       for (j = 0; j < col2; j++) {
          printf("Enter the Element b[%d][%d] : ", i, j);
          scanf("%d", &mat2[i][j]);
       }
-
-   //Subtraction of two matrices
    for (i = 0; i < row1; i++)
       for (j = 0; j < col1; j++) {
          mat3[i][j] = mat1[i][j] - mat2[i][j];
       }
 
-   //Print out the Resultant Matrix
    printf("\nThe Subtraction of two Matrices is : \n");
    for (i = 0; i < row1; i++) {
       for (j = 0; j < col1; j++) {
