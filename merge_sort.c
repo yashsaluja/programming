8e6180018dac555549e4dd5c62e64dc2edd87665
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-01-19 23:03:06 +0530

    program for merge sort

--- merge_sort.c
+++ merge_sort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #define max 10
 
 int a[11] = { 10, 14, 19, 26, 27, 31, 33, 35, 42, 44, 0 };
@@ -24,20 +25,24 @@ void merging(int low, int mid, int high) {
       a[i] = b[i];
 }
 
-void sort(int low, int high) {
+void sort(int low, int high)
+ {
    int mid;
    
-   if(low < high) {
+   if(low < high)
+    {
       mid = (low + high) / 2;
       sort(low, mid);
       sort(mid+1, high);
       merging(low, mid, high);
-   } else { 
+   } else
+    { 
       return;
    }   
 }
 
-int main() { 
+int main()
+ { 
    int i;
 
    printf("List before sorting\n");
