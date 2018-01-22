76dbb7d55d104ba1e5228799581654e4f47e1ae3
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-01-22 22:13:43 +0530

    program to find a leap year

--- merge_sort.c
+++ merge_sort.c
@@ -3,12 +3,15 @@
 #define max 10
 
 int a[11] = { 10, 14, 19, 26, 27, 31, 33, 35, 42, 44, 0 };
+
 int b[10];
 
-void merging(int low, int mid, int high) {
+void merging(int low, int mid, int high) 
+{
    int l1, l2, i;
 
-   for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++) {
+   for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++) 
+   {
       if(a[l1] <= a[l2])
          b[i] = a[l1++];
       else
