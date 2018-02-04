b50f9ee4e7ca84f63c6a6c32de103e6af1e6f5a3
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-02-04 23:12:50 +0530

    program for exponent series

--- exponent_series.c
+++ exponent_series.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 #define ACCURACY 0.0001
  
-int main() {
+int main()
+ {
    int n, count;
+
    float x, term, sum;
  
    printf("\nEnter value of x :");
+
    scanf("%f", &x);
  
    n = term = sum = count = 1;
  
-   while (n <= 100) {
+   while (n <= 100)
+    {
       term = term * x / n;
       sum = sum + term;
       count = count + 1;
