a349789b4954e624ac9b7fed45663dfa0435e98e
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-02-04 23:14:09 +0530

    program for generic linked list

--- generic_linked_list.c
+++ generic_linked_list.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 struct Node
 {
 
@@ -13,11 +14,14 @@ void push(struct Node** head_ref, void *new_data, size_t data_size)
     struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
  
     new_node->data  = malloc(data_size);
+
     new_node->next = (*head_ref);
  
     
     int i;
+
     for (i=0; i<data_size; i++)
+    
         *(char *)(new_node->data + i) = *(char *)(new_data + i);
  
     
