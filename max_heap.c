5a03f839a2a91a5837e0bcf785189802ac48f597
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-02-12 22:53:07 +0530

    program to find max heap

--- max_heap.c
+++ max_heap.c
@@ -1,17 +1,23 @@
 #include <iostream>
+
 #include <conio.h>
+
 using namespace std;
+
 void max_heapify(int *a, int i, int n)
 {
     int j, temp;
     temp = a[i];
     j = 2 * i;
+
     while (j <= n)
     {
         if (j < n && a[j+1] > a[j])
             j = j + 1;
+
         if (temp > a[j])
             break;
+
         else if (temp <= a[j])
         {
             a[j / 2] = a[j];
@@ -34,14 +40,18 @@ int main()
     int n, i, x;
     cout<<"enter no of elements of array\n";
     cin>>n;
+
     int a[20];
+
     for (i = 1; i <= n; i++)
     {
         cout<<"enter element"<<(i)<<endl;
         cin>>a[i];
     }
+
     build_maxheap(a,n);
     cout<<"Max Heap\n";
+    
     for (i = 1; i <= n; i++)
     {
         cout<<a[i]<<endl;
