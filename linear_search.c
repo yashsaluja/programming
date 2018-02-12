ec6ff92100ef372e7265ac9f4545afc52eeb0712
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-02-12 22:51:32 +0530

    program to find a linear search

--- linear_search.c
+++ linear_search.c
@@ -5,14 +5,17 @@ int main()
   int array[100], search, c, n;
 
   printf("Enter the number of elements in array\n");
+
   scanf("%d",&n);
 
   printf("Enter %d integer(s)\n", n);
 
   for (c = 0; c < n; c++)
+
      scanf("%d", &array[c]);
 
   printf("Enter the number to search\n");
+  
   scanf("%d", &search);
 
   for (c = 0; c < n; c++)
