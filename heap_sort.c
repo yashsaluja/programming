e21e432ed0301344f51ea9ac892d09f07497151f
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-02-22 23:07:34 +0530

    program for heap sort

--- heap_sort.c
+++ heap_sort.c
@@ -5,28 +5,37 @@ void main()
    int heap[10], no, i, j, c, root, temp;
 
    printf("\n Enter no of elements :");
+
    scanf("%d", &no);
+
    printf("\n Enter the nos : ");
+
    for (i = 0; i < no; i++)
       scanf("%d", &heap[i]);
+
    for (i = 1; i < no; i++)
    {
        c = i;
+
        do
        {
-           root = (c - 1) / 2;             
+           root = (c - 1) / 2;  
+
            if (heap[root] < heap[c])
            {
                temp = heap[root];
                heap[root] = heap[c];
                heap[c] = temp;
            }
+
            c = root;
-       } while (c != 0);
+
+       }
+        while (c != 0);
    }
 
    printf("Heap array : ");
-   
+
    for (i = 0; i < no; i++)
        printf("%d\t ", heap[i]);
 
