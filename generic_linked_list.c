6c22540319b7433a64b9966dc4988c18ad3430ea
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-02-22 23:09:37 +0530

    program for generic linked list

--- generic_linked_list.c
+++ generic_linked_list.c
@@ -53,16 +53,22 @@ int main()
  
     unsigned int_size = sizeof(int);
     int arr[] = {10, 20, 30, 40, 50}, i;
+
     for (i=4; i>=0; i--)
-       push(&start, &arr[i], int_size);
+
+    push(&start, &arr[i], int_size);
     printf("Created integer linked list is \n");
     printList(start, printInt);
  
     unsigned float_size = sizeof(float);
+
     start = NULL;
     float arr2[] = {10.1, 20.2, 30.3, 40.4, 50.5};
+
     for (i=4; i>=0; i--)
-       push(&start, &arr2[i], float_size);
+
+
+    push(&start, &arr2[i], float_size);
     printf("\n\nCreated float linked list is \n");
     printList(start, printFloat);
  
