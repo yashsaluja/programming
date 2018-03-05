c6f00a0572f634e0daa259dcfb15b17ee311a30d
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-03-05 23:07:45 +0530

    program to print fibonnaci series

--- Fibonnaci.c
+++ Fibonnaci.c
@@ -4,7 +4,9 @@ int main()
    int f1=0, f2=1, fib_ser, c=2, lmt;
  
    printf("Please enter the limit of the Fibonacci series :");
+
    scanf("%d",&lmt);
+
    printf("\nFibonacci series is: \n%d \n%d \n",f1,f2);
  
    while (c < lmt)
@@ -15,5 +17,6 @@ int main()
       f1=f2;
       f2=fib_ser;
    }
+   
    return 0;
 }
