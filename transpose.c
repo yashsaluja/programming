1ee6419583264f6e71559895040c8f9ca6024928
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2018-03-16 17:31:35 +0530

    program of transpose of a matrix

--- transpose.c
+++ transpose.c
@@ -5,15 +5,18 @@ int main()
   int m, n, c, d, matrix[10][10], transpose[10][10];
 
   printf("Enter the number of rows and columns of matrix\n");
+
   scanf("%d%d", &m, &n);
 
   printf("Enter the elements of matrix\n");
 
   for (c = 0; c < m; c++)
+
      for(d = 0; d < n; d++)
         scanf("%d",&matrix[c][d]);
 
   for (c = 0; c < m; c++)
+  
      for( d = 0 ; d < n ; d++ )
         transpose[d][c] = matrix[c][d];
 
